3cdd14712be08c4b1473e3110c435eb6d08870b5
Rob Broadley <git@rbroadley.co.uk>
2023-02-17 20:51:25 +0000

    Update adguard setup

--- adguard/create.sh
+++ adguard/create.sh
@@ -9,12 +9,22 @@ podman create --name $CONTAINER_NAME\
 	--restart unless-stopped\
 	-v $MOUNTS/work:/opt/adguardhome/work:Z\
 	-v $MOUNTS/conf:/opt/adguardhome/conf:Z\
+	--network home\
+	--ip $ADGUARD_IP\
+	--cap-add=NET_RAW\
 	-p 53:53/tcp -p 53:53/udp\
+	-p 67:67/udp -p 68:68/tcp -p 68:68/udp\
 	-p 80:80/tcp -p 443:443/tcp -p 443:443/udp -p 3000:3000/tcp\
+	--hostname $CONTAINER_NAME\
 	$IMAGE
 
-podman generate systemd \
-	--new --name $CONTAINER_NAME \
-	> /etc/systemd/system/$CONTAINER_NAME.service
+if [ "$1" == "--service" ]
+then
+	podman generate systemd \
+		--new --name $CONTAINER_NAME \
+		> /etc/systemd/system/$CONTAINER_NAME.service
 
-systemctl enable --now $CONTAINER_NAME
+	systemctl enable --now $CONTAINER_NAME
+else
+	podman start $CONTAINER_NAME
+fi
